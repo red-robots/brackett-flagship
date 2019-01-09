d0f6dfebf3e51697b41ba3e4c25b427389c33c6a
Austin Crane <austincrane@gmail.com>
2019-01-09 13:23:50 -0500

    add constant contact

--- sidebar-news.php
+++ sidebar-news.php
@@ -19,7 +19,11 @@
 				</div><!--.news-text-->
 			<?php endif;?>
 			<div class="news-contact-form">
-				<?php gravity_form( 1, false, false, false, '', false );?>
+				<?php //gravity_form( 1, false, false, false, '', false );?>
+				<!-- Begin Constant Contact Active Forms -->
+				<script> var _ctct_m = "9c3822df0b3a391dd4fa42a1f1b95cc9"; </script>
+				<script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+				<!-- End Constant Contact Active Forms -->
 			</div><!--.news-contact-form-->
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
