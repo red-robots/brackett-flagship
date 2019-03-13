2aea6984abe821ea7d768af2622cf911168225af
Austin Crane <austincrane@gmail.com>
2019-03-13 13:26:42 -0400

    add FB pixel

--- header.php
+++ header.php
@@ -30,6 +30,23 @@
 
     <![endif]-->
     <script src="https://use.fontawesome.com/4945cee666.js"></script>
+
+    <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '253750215575184');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=253750215575184&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+
 <?php wp_head(); ?>
 
 <script> var _ctct_m = "9c3822df0b3a391dd4fa42a1f1b95cc9"; </script>
